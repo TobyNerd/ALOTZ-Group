0c0251680f12f853d1926e2bcb947bc66338d872
Zaki Hussain <m-zaki@live.dk>
2018-03-20 23:00:42 +0100

    more cleanup

---
+++ Interface&Navigation/header.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <!-- Within the head tag we put edit styles with CSS and put key info like title of the webpage' -->
+  <head>
+    <meta charset="utf-8">
+
+    <!-- the title is what appears on the actual tab within the browser -->
+    <title>
+      Physiosearch
+    </title>
+
+    <!-- ^ "imports" a stylesheet from the link, which contains a number of
+    icons, we reffer to it in the body as 'fa fa-search' -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <!-- ^ the link tag defines the browser tab image, here we use our
+    zakilicious logo -->
+    <link rel="icon" href="https://i.imgur.com/0CVNTap.png">
+
+    <!-- ^ "imports" the main CSS stylesheet from the local link -->
+    <link type="text/css" rel="stylesheet" href="style.css"/>
+
+  </head>
+  <body>
+
+  </body>
+</html>
