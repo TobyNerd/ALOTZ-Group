87b805f919566fcc743892098836f5ea1be2791f
Zaki Hussain <m-zaki@live.dk>
2018-03-22 09:38:46 +0100

    added header

---
+++ Interface&Navigation/header.php
@@ -0,0 +1,9 @@
+<nav>
+  <div class="nav-wrapper">
+    <a href="#" class="brand-logo"><img src="https://i.imgur.com/0CVNTap.png" height="45px" alt=""></a>
+    <ul id="nav-mobile" class="right hide-on-med-and-down">
+      <li><a href="#">Become a physiotherapist</a></li>
+      <li><a href="#">Sign in</a></li>
+    </ul>
+  </div>
+</nav>
--- Interface&Navigation/index.php
+++ Interface&Navigation/index.php
@@ -32,15 +32,8 @@
     </head>
 
     <body>
-      <nav>
-        <div class="nav-wrapper">
-          <a href="#" class="brand-logo"><img src="https://i.imgur.com/0CVNTap.png" height="45px" alt=""></a>
-          <ul id="nav-mobile" class="right hide-on-med-and-down">
-            <li><a href="#">Become a physiotherapist</a></li>
-            <li><a href="#">Sign in</a></li>
-          </ul>
-        </div>
-      </nav>
+
+      <?php include 'header.php' ?>
 
       <div class="container center cover">
         <div class="row">
