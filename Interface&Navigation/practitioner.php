3710a72846675248fa01ac652d97e8f2d17095cc
Zaki Hussain <m-zaki@live.dk>
2018-03-23 13:17:29 +0100

    update to header

--- Interface&Navigation/practitioner.php
+++ Interface&Navigation/practitioner.php
@@ -14,14 +14,18 @@
 
       <?php include 'header.php' ?>
 
-      <img src="https://i.imgur.com/0CVNTap.png">
+      <div class="container">
+        <div class="row">
 
-      <h3>Physiosearch 2nd page</h3>
+          <h3>Physiosearch 2nd page</h3>
 
-      <p>
-        Allowing you to easily search for physiotherapists based on
-        <br /> costumer reviews, diplomas and special competences.
-      </p>
+          <p>
+            Allowing you to easily search for physiotherapists based on
+            <br /> costumer reviews, diplomas and special competences.
+          </p>
+
+        </div>
+      </div>
 
     </body>
 </html>
