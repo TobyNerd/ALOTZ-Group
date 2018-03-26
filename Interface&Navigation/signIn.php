649088ddb2b5479666b10065caf701485f291a3e
TobyNerd <32539698+TobyNerd@users.noreply.github.com>
2018-03-26 12:52:57 +0200

    Implemented hyperlinks for page to page navigation.
    Attached index.php hyperlink to the brand logo. All changes made in header.php

---
+++ Interface&Navigation/signIn.php
@@ -0,0 +1,17 @@
+<!-- It all starts with the html tag, better respect yo' -->
+<html>
+
+    <!-- Within the head tag we put edit styles with CSS and put key info like title of the webpage' -->
+    <head>
+      <meta charset="utf-8">
+
+      <!-- the title is what appears on the actual tab within the browser -->
+      <title>
+      SignIn
+      </title>
+
+    </head>
+
+    <body>
+      <!-- we include the php script header.php which contains pictures like the logo and background --> 
+      <?php include 'header.php' ?>
