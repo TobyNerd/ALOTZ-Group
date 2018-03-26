4ff40b004e5976c45662f7926f1c67c3936060d5
TobyNerd <32539698+TobyNerd@users.noreply.github.com>
2018-03-26 13:31:52 +0200

    Set up of E-mail and Password prompt on signin.php
    Confused on how to reduce the width of the two columns that contains the prompts. ZAKI HALP ME PLS

--- Interface&Navigation/practitioner.php
+++ Interface&Navigation/practitioner.php
@@ -14,14 +14,5 @@
 
       <?php include 'header.php' ?>
 
-      <img src="https://i.imgur.com/0CVNTap.png">
-
-      <h3>Physiosearch 2nd page</h3>
-
-      <p>
-        Allowing you to easily search for physiotherapists based on
-        <br /> costumer reviews, diplomas and special competences.
-      </p>
-
     </body>
 </html>
--- Interface&Navigation/signIn.php
+++ Interface&Navigation/signIn.php
@@ -1,3 +1,5 @@
+<!doctype html>
+
 <!-- It all starts with the html tag, better respect yo' -->
 <html>
 
@@ -7,7 +9,7 @@
 
       <!-- the title is what appears on the actual tab within the browser -->
       <title>
-      SignIn
+      Physiosearch
       </title>
 
     </head>
@@ -15,3 +17,38 @@
     <body>
       <!-- we include the php script header.php which contains pictures like the logo and background --> 
       <?php include 'header.php' ?>
+
+      <div class="container center cover">
+        <div class="row">
+
+           <!-- Adds the central text above searchbar. It's ID is "h1" as its a h1 type header.--> 
+          <div class="col m12">
+            <h1>Sign In</h1>
+          </div>
+        </div>
+        
+          <!-- Adds the stylized search bar. It's ID is "search-bar-cover" --> 
+          <div class="col m4">
+            <input placeholder="E-mail" id="email-prompt" type="text" class="validate center">
+            <label for="login-cover"></label>
+          </div>
+          <div class="col m4">
+            <input placeholder="Password" id="password-prompt" type="text" class="validate center">
+            <label for="login-cover"></label>
+          </div>
+          
+          <!-- Adds the stylized button "search" --> 
+          <div class="col m12">
+            <button class="btn waves-effect waves-light" type="submit" name="action">Sign In
+
+            </button>
+          </div>
+
+        </div>
+      </div>
+
+    <div class="backgroundImage">
+    </div>
+
+    </body>
+</html>
