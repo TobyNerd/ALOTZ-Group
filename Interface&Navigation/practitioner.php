96f116aff928454ef68f5fc3b5217cddbdce5a77
TobyNerd <32539698+TobyNerd@users.noreply.github.com>
2018-03-26 11:25:49 +0200

    Revert "Revert "Revert "Merge branch 'master' of https://github.com/TobyNerd/ALOTZ-Group"""
    This reverts commit 714f47e9dadcbeb0b7a57aef82923c76ef37443f.

---
+++ Interface&Navigation/practitioner.php
@@ -0,0 +1,27 @@
+<!doctype html>
+
+<!-- It all starts with the html tag, better respect yo' -->
+<html>
+    <head>
+
+      <title>
+      Physiosearch
+      </title>
+
+    </head>
+
+    <body>
+
+      <?php include 'header.php' ?>
+
+      <img src="https://i.imgur.com/0CVNTap.png">
+
+      <h3>Physiosearch 2nd page</h3>
+
+      <p>
+        Allowing you to easily search for physiotherapists based on
+        <br /> costumer reviews, diplomas and special competences.
+      </p>
+
+    </body>
+</html>
